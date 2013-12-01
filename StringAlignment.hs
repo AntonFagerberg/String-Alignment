8b146b200d6879512b70ac978ba1af42f0ec3072
Anton Fagerberg <anton@antonfagerberg.com>
2013-12-02 00:34:39 +0100

    Finished it

--- StringAlignment.hs
+++ StringAlignment.hs
@@ -14,9 +14,6 @@ string2 = "vintner"
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
 attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
 
-tupleScore :: AlignmentType -> Int
-tupleScore (s1, s2) = sum . zipWith score s1 $ s2
-
 -- Get the or every item in the list which has the highest value
 -- according to the valueFcn function.
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
@@ -32,8 +29,7 @@ score x y
   | x == y               = scoreMatch
   | otherwise            = scoreMismatch
 
--- Calculate the similiarity score of two strings.
--- If they are not of the same length, let it crash.
+-- Calculate the similiarity score of two strings NOT using memoization.
 similarityScoreUnoptimized :: String -> String -> Int
 similarityScoreUnoptimized [] []       = 0
 similarityScoreUnoptimized (s1:ss1) [] = score s1 '-' + similarityScoreUnoptimized ss1 []
@@ -43,6 +39,7 @@ similarityScoreUnoptimized (s1:ss1) (s2:ss2) =
            score s1 '-' + similarityScoreUnoptimized ss1 (s2:ss2),
            score '-' s2 + similarityScoreUnoptimized (s1:ss1) ss2]
 
+-- Calculate the similarity score of two strings using memoization.
 similarityScore :: String -> String -> Int
 similarityScore s1 s2 = sScore (length s1) (length s2)
   where
@@ -50,17 +47,19 @@ similarityScore s1 s2 = sScore (length s1) (length s2)
     sScore x y = sTable !! x !! y
     sTable = [[ sEntry x y | y <- [0..] ] | x <- [0..]]
 
+    cx, cy :: Int -> Char
+    cx x = s1 !! (x - 1)
+    cy y = s2 !! (y - 1)
+
     sEntry :: Int -> Int -> Int
     sEntry 0 0 = 0
-    sEntry x 0 = score (s1 !! (x - 1)) '-' + sScore (x - 1) 0
-    sEntry 0 y = score '-' (s2 !! (y - 1)) + sScore 0 (y - 1)
-    sEntry x y = maximum [score cx cy  + sScore (x - 1) (y - 1),
-                          score cx '-' + sScore (x - 1) y,
-                          score '-' cy + sScore x (y - 1)]
-                            where
-                              cx = s1 !! (x - 1)
-                              cy = s2 !! (y - 1)
+    sEntry x 0 = score (cx x) '-' + sScore (x - 1) 0
+    sEntry 0 y = score '-' (cy y) + sScore 0 (y - 1)
+    sEntry x y = maximum [score (cx x) (cy y) + sScore (x - 1) (y - 1),
+                          score (cx x) '-'    + sScore (x - 1) y,
+                          score '-' (cy y)    + sScore x (y - 1)]
 
+-- Calculate the optimal string alignment NOT using memoization.
 optAlignmentsUnoptimized :: String -> String -> [AlignmentType]
 optAlignmentsUnoptimized [] []         = [([], [])]
 optAlignmentsUnoptimized (x:xs) []     = attachHeads x '-' $ optAlignmentsUnoptimized xs []
@@ -69,15 +68,20 @@ optAlignmentsUnoptimized (x:xs) (y:ys) =
   maximaBy tupleScore . concat $ [attachHeads x y   $ optAlignmentsUnoptimized xs ys,
                                   attachHeads x '-' $ optAlignmentsUnoptimized xs (y:ys),
                                   attachHeads '-' y $ optAlignmentsUnoptimized (x:xs) ys]
+    where 
+      tupleScore :: AlignmentType -> Int
+      tupleScore (s1, s2) = sum . zipWith score s1 $ s2
 
+-- Calculate optimal string alignment using memoization.
 optAlignments :: String -> String -> [AlignmentType]
-optAlignments s1 s2 = [(reverse x, reverse y) | (x, y) <- snd . optA (length s1) $ (length s2)]
+optAlignments s1 s2 = [(reverse r1, reverse r2) | (r1, r2) <- snd . optA (length s1) $ length s2]
   where
-    cx x = s1 !! (x - 1)
-    cy y = s2 !! (y - 1)
-
     add :: Char -> Char -> (Int, [AlignmentType]) -> (Int, [AlignmentType])
     add h1 h2 (i, a) = ((score h1 h2) + i, attachHeads h1 h2 a)
+
+    cx, cy :: Int -> Char
+    cx x = s1 !! (x - 1)
+    cy y = s2 !! (y - 1)
 
     optA :: Int -> Int -> (Int, [AlignmentType])
     optA x y = oTable !! x !! y
@@ -92,5 +96,10 @@ optAlignments s1 s2 = [(reverse x, reverse y) | (x, y) <- snd . optA (length s1)
                                   add (cx x) '-'    $ optA (x - 1) y,
                                   add '-' (cy y)    $ optA x (y - 1)]
 
-
-
+-- Output all number of optimal aligments between two strings.
+outputOptAlignments :: String -> String -> IO ()
+outputOptAlignments string1 string2 = do
+  let results = optAlignments string1 string2
+  putStrLn $ "There are " ++ (show . length $ results) ++ " optimal alignments!"
+  mapM_ (\(r1, r2) -> putStrLn $ "\n" ++ r1 ++ "\n" ++ r2) results
+  return ()
